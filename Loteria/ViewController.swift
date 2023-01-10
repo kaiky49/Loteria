fa515c197829a6e4c6d858aaf2ff0e0730167f6c
Rafael Nicolau <rafaelidn27@gmail.com>
2023-01-10 17:40:36 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Rafael Nicolau on 10/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
